bf4c8e6dc0cfea5f73d4f65c307df57f6c385e7a
AC <ralfer@users.noreply.github.com>
2016-07-15 16:41:17 +0000

    polished outputs

--- generate_outputs.R
+++ generate_outputs.R
@@ -12,17 +12,15 @@ library(gridExtra)
 library(sampling)
 library(tables)
 
-rm(list=ls())
 
 #for (ext in c('html','word')){
 for (i in list.files('outputs','.RData',full.names = T)){
   print(i)
   prefix <- file_path_sans_ext(basename(i))
-  opts_chunk$set(fig.path=paste0('outputs/figures/',prefix))
   env = new.env()
-  load(i, env)
+  assign('fname',i, env)
   #assign('sim_data', sim_data, envir = env)
-  render('summaries_and_plots.Rmd',output_dir	= 'outputs', output_file = paste0('outputs/',prefix,c('.html','.docx')), output_format=c('html_document', 'word_document'),output_options = list(keep_md	=T), envir=env, quiet=T)
+  rmarkdown::render('summaries_and_plots.Rmd',output_dir	= 'outputs', output_file = paste0('outputs/',prefix,c('.html','.docx')), output_format=c('html_document', 'word_document'),output_options = list(keep_md = T), envir=env, quiet=F)
   
 }
 #}
